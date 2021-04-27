02b6106004552ea0c10f1e2f5ebdc8f4e12b4868
che05 <79547794+che05@users.noreply.github.com>
2021-04-27 20:11:33 +1000

    Update TRON.py
    screen works

--- TRON.py
+++ TRON.py
@@ -3,15 +3,22 @@ pygame.init()
 pygame.display.set_caption('TRON')
 
 #VARIABLES 
-WIDTH = 1200
+WIDTH = 1000
 HEIGHT = 600
+BORDER = 590
 
 #screen 
-screen = pygame.dislpay.set_mode((WIDTH, HEIGHT))
+bgColor = pygame.Color("black")
+fgColor = pygame.Color("yellow")
+screen = pygame.display.set_mode((WIDTH, HEIGHT)) 
+pygame.draw.rect(screen, fgColor, pygame.Rect(200,40,HEIGHT,5))
+pygame.draw.rect(screen, fgColor, pygame.Rect(200,550,HEIGHT,5))
+pygame.draw.rect(screen, fgColor, pygame.Rect(200,40,5,510))
+pygame.draw.rect(screen, fgColor, pygame.Rect(800,40,5,515))
 
 running = True
 while running:
     pygame.display.flip()
     for event in pygame.event.get():
-        if event.type == pygmae.QUIT:
+        if event.type == pygame.QUIT:
             running = False

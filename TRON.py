43e1d5220f2354b636b16508c65b7e8f874d1fbf
che05 <79547794+che05@users.noreply.github.com>
2021-04-29 09:55:32 +1000

    Update TRON.py
    dont work

--- TRON.py
+++ TRON.py
@@ -6,6 +6,36 @@ pygame.display.set_caption('TRON')
 WIDTH = 1000
 HEIGHT = 600
 BORDER = 590
+direction = 1
+
+class player1: #no clue what im doing
+    PLAYER = 4
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+        self.speed = 1
+        #self.direction = d1
+
+    def draw(self, color):
+        global screen
+        pygame.draw.rect(screen,color, (self.x, self.y), self.PLAYER)
+
+    def update(self, color):
+        global bgColor
+        self.show(bgColor)
+
+#class player2:
+
+    #def __init__(self,x,y):
+       # self.x = x
+        #self.y = y
+        #self.speed = 1
+        #self.direction = d2
+         
+
+
+#playerbody
+playerbody = player1(WIDTH - player1.PLAYER, HEIGHT // 2, )
 
 #screen 
 bgColor = pygame.Color("black")
@@ -15,6 +45,10 @@ pygame.draw.rect(screen, fgColor, pygame.Rect(200,40,HEIGHT,5))
 pygame.draw.rect(screen, fgColor, pygame.Rect(200,550,HEIGHT,5))
 pygame.draw.rect(screen, fgColor, pygame.Rect(200,40,5,510))
 pygame.draw.rect(screen, fgColor, pygame.Rect(800,40,5,515))
+
+playerbody.draw(pygame("blue"))
+print('player')
+
 
 running = True
 while running:

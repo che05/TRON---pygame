00b557ceadf195192c8538835ef47a3de0dcbd05
che05 <79547794+che05@users.noreply.github.com>
2021-05-05 09:54:01 +1000

    Update TRON.py

--- TRON.py
+++ TRON.py
@@ -6,36 +6,41 @@ pygame.display.set_caption('TRON')
 WIDTH = 1000
 HEIGHT = 600
 BORDER = 590
-direction = 1
+VELOCITY = 1
 
-class player1: #no clue what im doing
+class player: #no clue what im doing
     PLAYER = 4
-    def __init__(self,x,y):
+    def __init__(self,x,y,vx,vy,):
         self.x = x
         self.y = y
+        self.vx = vx
+        self.vy = vy
         self.speed = 1
-        #self.direction = d1
+        #self.direction = d
+        
 
-    def draw(self, color):
+    def show(self, color):
         global screen
-        pygame.draw.rect(screen,color, (self.x, self.y), self.PLAYER)
+        pygame.draw.rect(screen,color, pygame.Rect(self.x,self.y, player.PLAYER, player.PLAYER))
+    
 
     def update(self, color):
         global bgColor
-        self.show(bgColor)
+        self.x = self.x + self.vx
+        self.y = self.y + self.vy
+        self.show(color)
 
-#class player2:
-
-    #def __init__(self,x,y):
-       # self.x = x
-        #self.y = y
-        #self.speed = 1
-        #self.direction = d2
-         
+    def move(self):
+        if self.x <=0:
+            self.x += self.direction[0]
+            self.y += self.direction[1]
+        else:
+            self.x += self.direction[0] * 2
+            self.y += self.direction[1] * 2
 
 
 #playerbody
-playerbody = player1(WIDTH - player1.PLAYER, HEIGHT // 2, )
+player1 = player(HEIGHT // 2, HEIGHT  // 2, (2,0), 1)
 
 #screen 
 bgColor = pygame.Color("black")
@@ -46,9 +51,21 @@ pygame.draw.rect(screen, fgColor, pygame.Rect(200,550,HEIGHT,5))
 pygame.draw.rect(screen, fgColor, pygame.Rect(200,40,5,510))
 pygame.draw.rect(screen, fgColor, pygame.Rect(800,40,5,515))
 
-playerbody.draw(pygame("blue"))
+player1.show(pygame.Color('white'))
 print('player')
 
+
+#keys=pygame.key.get_pressed()
+#if keys[K_LEFT]:
+#    direction-=1
+#    if direction==-1:
+#        direction=0
+#if keys[K_RIGHT]:
+#    direction+=1
+#    if direction==5:
+#        direction=4    
+#if keys[K_UP]:
+#    direction
 
 running = True
 while running:

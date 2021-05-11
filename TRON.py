e3854a0f5da3d407e06d7ced51fc9f7f8587084f
che05 <79547794+che05@users.noreply.github.com>
2021-05-11 11:35:52 +1000

    Update TRON.py
    2player

--- TRON.py
+++ TRON.py
@@ -6,18 +6,17 @@ pygame.display.set_caption('TRON')
 WIDTH = 1000
 HEIGHT = 600
 BORDER = 590
-VELOCITY = 0.3
+VELOCITY = 0.2
 
 class player: #no clue what im doing
-    PLAYER = 4
-    def __init__(self,x,y,vx,vy,color):
+    PLAYER = 5
+    def __init__(self,x,y,vx,vy,color,controls):
         self.x = x
         self.y = y
         self.vx = vx
         self.vy = vy
         self.playerColor = color
-        #self.direction = d
-        
+        self.controls = controls
 
     def show(self, color):
         global screen
@@ -30,35 +29,31 @@ class player: #no clue what im doing
         nextX = self.x + self.vx
         nextY = self.y + self.vy
         
-
         self.show(bgColor)
         self.x = nextX
         self.y = nextY
         self.show(self.playerColor)
 
-        
-        
-
     def move(self,direction):
         
-        print("keys")
-        if direction == pygame.K_LEFT:
-            self.vx = - VELOCITY
-            self.vy = 0
-        elif direction == pygame.K_RIGHT:
-            self.vx = VELOCITY
-            self.vy = 0
-        elif direction == pygame.K_UP:
-            self.vx = 0
-            self.vy = - VELOCITY
-        elif direction == pygame.K_DOWN:
-            self.vx = 0
-            self.vy = VELOCITY
-        print("keys2")
+        #
+            if direction == self.controls[0]:
+                self.vx = - VELOCITY
+                self.vy = 0
+            elif direction == self.controls[1]:
+                self.vx = VELOCITY
+                self.vy = 0
+            elif direction == self.controls[2]:
+                self.vx = 0
+                self.vy = - VELOCITY
+            elif direction == self.controls[3]:
+                self.vx = 0
+                self.vy = VELOCITY
+        
 
 #playerbody
-player1 = player(HEIGHT // 2, HEIGHT  // 2, 2, 0,(pygame.Color('blue')))
-
+player1 = player(HEIGHT // 2, HEIGHT  // 2, 0.3, 0,(pygame.Color('blue')), [pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN])
+player2 = player(HEIGHT // 2, HEIGHT  // 2, -0.3, 0,(pygame.Color('red')), [pygame.K_a, pygame.K_d, pygame.K_w, pygame.K_s])
 #screen 
 bgColor = pygame.Color("black")
 fgColor = pygame.Color("yellow")
@@ -69,7 +64,9 @@ pygame.draw.rect(screen, fgColor, pygame.Rect(200,40,5,510))
 pygame.draw.rect(screen, fgColor, pygame.Rect(800,40,5,515))
 
 player1.show(pygame.Color('blue'))
-print('player')
+print('player1')
+player2.show(pygame.Color('red'))
+print('player2')
 
 
 
@@ -78,8 +75,10 @@ running = True
 while running:
     pygame.display.flip()
     player1.update()
+    player2.update()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN:
             player1.move(event.key)
+            player2.move(event.key)

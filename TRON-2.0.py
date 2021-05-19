6350e68a9222af59e71e5155e166b6bafddb6ab4
che05 <79547794+che05@users.noreply.github.com>
2021-05-19 10:32:37 +1000

    Update TRON-2.0.py

--- TRON-2.0.py
+++ TRON-2.0.py
@@ -1,16 +1,18 @@
-import pygame
+import pygame, time
 pygame.init()
 pygame.display.set_caption('TRON')
 
 #VARIABLES 
-WIDTH = 1000
-HEIGHT = 600
-BORDER = 590
-VELOCITY = 0.2
+WIDTH = 600
+HEIGHT = 500
+BORDER = 580
+VELOCITY = 1
+
 
 class player: #no clue what im doing
     PLAYER = 5
-    def __init__(self,x,y,vx,vy,color,controls,tail):
+    def __init__(self,name,x,y,vx,vy,color,controls,tail):
+        self.name = name
         self.x = x
         self.y = y
         self.vx = vx
@@ -23,15 +25,28 @@ class player: #no clue what im doing
         global screen
         pygame.draw.rect(screen, color, pygame.Rect(self.x,self.y, player.PLAYER, player.PLAYER))
     
-
-    def update(self):
-        global bgColor, VELOCITY
+    def update(self,opponentTail):
+        global bgColor, VELOCITY, nextX, nextY
 
         self.tail.append((self.x, self.y))
 
         nextX = self.x + self.vx
         nextY = self.y + self.vy
-        
+
+        if (nextX, nextY) in self.tail:
+            print(self.name + " stop hitting yourself.")
+            running == False
+        elif (nextX, nextY) in opponentTail:
+            print(" stop hitting your opponent!")
+            running == False
+        if nextX < 10 or nextX > WIDTH - 10
+            print(self.name + " hit the left or right wall.")
+            screen.fill("black")
+            
+        if nextY < 10 or nextY > HEIGHT - 10:
+            print(self.name + " hit the top or bottom wall.")
+            
+               
         #self.show(bgColor)
         self.x = nextX
         self.y = nextY
@@ -39,49 +54,58 @@ class player: #no clue what im doing
 
     def move(self,direction):
         
-        #
-            if direction == self.controls[0]:
-                self.vx = - VELOCITY
-                self.vy = 0
-            elif direction == self.controls[1]:
-                self.vx = VELOCITY
-                self.vy = 0
-            elif direction == self.controls[2]:
-                self.vx = 0
-                self.vy = - VELOCITY
-            elif direction == self.controls[3]:
-                self.vx = 0
-                self.vy = VELOCITY
-        
+        if direction == self.controls[0]:
+            self.vx = - VELOCITY
+            self.vy = 0
+        elif direction == self.controls[1]:
+            self.vx = VELOCITY
+            self.vy = 0
+        elif direction == self.controls[2]:
+            self.vx = 0
+            self.vy = - VELOCITY
+        elif direction == self.controls[3]:
+            self.vx = 0
+            self.vy = VELOCITY
 
+    def new_game():
+        newP1 = player("blue", HEIGHT // 2 - 200, HEIGHT  // 2, 1, 0,(pygame.Color('blue')), [pygame.K_a, pygame.K_d, pygame.K_w, pygame.K_s], [])
+        newP2 = player("red", HEIGHT // 2 + 300, HEIGHT  // 2, -1, 0,(pygame.Color('red')), [pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN], [])
+
+    def endGame(self):
+        running == False
+        
+        
+gameSpeed = pygame.time.Clock()
 #playerbody
-player1 = player(HEIGHT // 2, HEIGHT  // 2, 0.3, 0,(pygame.Color('blue')), [pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN], [])
-#player2 = player(HEIGHT // 2, HEIGHT  // 2, -0.3, 0,(pygame.Color('red')), [pygame.K_a, pygame.K_d, pygame.K_w, pygame.K_s])
+player1 = player("blue", HEIGHT // 2 - 200, HEIGHT  // 2, 1, 0,(pygame.Color('blue')), [pygame.K_a, pygame.K_d, pygame.K_w, pygame.K_s], [])
+player2 = player("red", HEIGHT // 2 + 300, HEIGHT  // 2 , -1, 0,(pygame.Color('red')), [pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN], [])
 #screen 
 bgColor = pygame.Color("black")
 fgColor = pygame.Color("yellow")
 screen = pygame.display.set_mode((WIDTH, HEIGHT)) 
-pygame.draw.rect(screen, fgColor, pygame.Rect(200,40,HEIGHT,5))
-pygame.draw.rect(screen, fgColor, pygame.Rect(200,550,HEIGHT,5))
-pygame.draw.rect(screen, fgColor, pygame.Rect(200,40,5,510))
-pygame.draw.rect(screen, fgColor, pygame.Rect(800,40,5,515))
-
+pygame.draw.rect(screen, fgColor, pygame.Rect(5,10,BORDER,5))
+pygame.draw.rect(screen, fgColor, pygame.Rect(5,495,BORDER,5))
+pygame.draw.rect(screen, fgColor, pygame.Rect(5,10,5,BORDER))
+pygame.draw.rect(screen, fgColor, pygame.Rect(585,10,5,BORDER))
+    
 player1.show(pygame.Color('blue'))
 print('player1')
-#player2.show(pygame.Color('red'))
-#print('player2')
+player2.show(pygame.Color('red'))
+print('player2')
 
-
-
+display_surface = pygame.display.set_mode((200, 40))
+textRect.center = (WIDTH // 2, HEIGHT // 2)
 
 running = True
 while running:
     pygame.display.flip()
-    player1.update()
-    #player2.update()
+    gameSpeed.tick(120)
+    player1.update(player2.tail)
+    player2.update(player1.tail)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN:
             player1.move(event.key)
-            #player2.move(event.key)
+            player2.move(event.key)
+        
